a97eb40875316406b707e917bcee15a5a16db5a3
gitblairshub <blairsarlo@gmail.com>
2022-02-02 14:22:41 -0500

    Added assets folder with a cold and hot pic to display with according weather from api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const api = {
   key: "ed0416881a06725f3290f9ca566caa3a",
-  base: "https://api.openweathermap.org/data/2.5/C"
+  base: "https://api.openweathermap.org/data/2.5/C2022-02-02-14-10-38.png2022-02-02-14-11-08.png"
 }
 
 function App() {
@@ -14,3 +14,4 @@ function App() {
 }
 
 export default App;
+2022-02-02-14-20-10.png
