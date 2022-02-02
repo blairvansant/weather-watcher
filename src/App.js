341b9cd0db37bf8c6c1d73331b421a2b5c83c82c
gitblairshub <blairsarlo@gmail.com>
2022-02-02 12:45:17 -0500

    Add openweathermap api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const api = {
+  key: "ed0416881a06725f3290f9ca566caa3a",
+  base: "https://api.openweathermap.org/data/2.5/C"
+}
+
 function App() {
   return (
     <div className="App">
